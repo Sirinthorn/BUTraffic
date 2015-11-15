535f4caced575720e8967a02e1058cb24a14d946
sirinthorn <sirinthorn.c@bu.ac.th>
2015-11-15 11:12:22 +0700

    ListView Finished

---
+++ app/src/main/java/com/bu/sirinthorn/butraffic1/DeTailActivity.java
@@ -0,0 +1,13 @@
+package com.bu.sirinthorn.butraffic1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeTailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_de_tail);
+    }
+}
--- app/src/main/java/com/bu/sirinthorn/butraffic1/MyAdapter.java
+++ app/src/main/java/com/bu/sirinthorn/butraffic1/MyAdapter.java
@@ -1,9 +1,12 @@
 package com.bu.sirinthorn.butraffic1;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by student on 11/14/2015.
@@ -21,15 +24,11 @@ public class MyAdapter extends BaseAdapter{
     public MyAdapter(Context objContext, String[] titleStrings, int[] iconInts) {
         this.objContext = objContext;
         this.titleStrings = titleStrings;
-        this.iconInts = iconInts;
-
-
-
-    }//Constructor
+        this.iconInts = iconInts;    }//Constructor
 
     @Override
     public int getCount() {
-        return 0;
+        return titleStrings.length;
     }
 
     @Override
@@ -44,6 +43,17 @@ public class MyAdapter extends BaseAdapter{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.my_listview, parent, false);
+
+        //setup Title
+        TextView titlTextView = (TextView) objView1.findViewById(R.id.textView2);
+        titlTextView.setText(titleStrings[position]);
+        //setup Icon or image in listview
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imageView);
+        iconImageView.setImageResource(iconInts[position]);
+
+
+        return objView1;
     }
 }//Main Class
